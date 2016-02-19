2781c36f5ff92fe504e3f07360c6dfa6ea6d0bc7
anish walawalkar <anish@jewlr.com>
2016-02-19 15:17:37 -0500

    got holidays for jewlr and bogarz working

--- lib/factory_days.rb
+++ lib/factory_days.rb
@@ -1,5 +1,29 @@
-require "factory_days/version"
+require_relative "factory_days/version"
+require_relative "factory_days/factory_days"
+require_relative "holidays"
 
 module FactoryDays
-  # Your code goes here...
+  def factoryday?
+  end
+
+  def next_factoryday
+  end
+
+  def prev_factoryday
+  end
+
+  def factorydays_until
+  end
+
+  def factorydays_passed
+  end
+end
+
+
+class Date
+  include FactoryDays
+end
+
+class Time
+  include FactoryDays
 end
